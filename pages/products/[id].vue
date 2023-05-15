feb43abef2f94552618ed8c5d2c153b4cca3b769
Nazmul Haque <bpinazmul18@gmail.com>
2023-05-15 10:46:02 +0600

    ADD: Routes parameter.

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Product details for {{ id }}</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, eaque
+      quod! Quibusdam quo veritatis vitae voluptas at? Nihil, natus aliquam.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style lang="scss" scoped></style>
