1a031c1541ed658b06be9f4558bd45b5ca091c41
Nazmul Haque <bpinazmul18@gmail.com>
2023-05-16 08:35:09 +0600

    ADD: Add header bottom border.

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="shadow-sm bg-white">
+  <header class="shadow-sm bg-white border-b border-emerald-500">
     <nav class="container mx-auto p-4 flex items-center justify-between">
       <BrandLogo />
       <ul class="flex items-center justify-between gap-6">
@@ -22,6 +22,24 @@
             to="/about"
             class="font-normal text-base text-slate-500 leading-4 hover:border-b border-emerald-500 py-2 px-1 transition"
             >About</NuxtLink
+          >
+        </li>
+      </ul>
+
+      <ul class="flex items-center gap-2">
+        <li>
+          <NuxtLink
+            to="/login"
+            class="font-normal text-base text-white leading-4 transition bg-emerald-500 border border-emerald-500 hover:bg-white hover:text-emerald-500 py-2 px-4 rounded"
+            >Login</NuxtLink
+          >
+        </li>
+        /
+        <li>
+          <NuxtLink
+            to="/register"
+            class="font-normal text-base text-emerald-500 leading-4 transition bg-white border border-emerald-500 hover:bg-emerald-500 hover:text-white py-2 px-4 rounded"
+            >Register</NuxtLink
           >
         </li>
       </ul>
