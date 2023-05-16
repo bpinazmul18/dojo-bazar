94e67fa85cd1f579bf701ac0973e08814c367b91
Nazmul Haque <bpinazmul18@gmail.com>
2023-05-16 07:08:59 +0600

    REMOVE: Remove all extra line of code.

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -3,7 +3,3 @@
     <h4>Footer</h4>
   </footer>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -16,15 +16,3 @@
     </nav>
   </header>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  setup() {
-    return {};
-  }
-});
-</script>
-
-<style scoped></style>
