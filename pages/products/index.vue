b5cc62922643846529cd53ebd531431882fcb34b
Nazmul Haque <bpinazmul18@gmail.com>
2023-05-20 17:27:28 +0600

    ADD: Add padding on top and bottom.

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-10">
+  <div>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="product in products" :key="product.id">
         <ProductCard :product="product"/>
